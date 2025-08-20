e25807342972c010af921e3809f66fb467cc886c
Odi2520 <Odidestiny12@gmail.com>
2025-08-20 18:26:00 +0100

    Update settings.py

--- giveaway/settings.py
+++ giveaway/settings.py
@@ -112,8 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# api/settings.py
-WSGI_APPLICATION = 'giveaway.wsgi.app'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -126,6 +125,7 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
