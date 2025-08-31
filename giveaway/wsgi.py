34351d17901f927bcc1e7fff474ea6f3aa15c4f3
Odi2520 <Odidestiny12@gmail.com>
2025-08-31 21:20:19 +0100

    Update wsgi.py

--- giveaway/wsgi.py
+++ giveaway/wsgi.py
@@ -13,7 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giveaway.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
+
 
 
 
